9852d40510978004ca2766c45425e4f59c9e2b3c
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-27 14:29:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(arr){
+    var winner = arr.find((element)=>{
+        return element.result === 'W';
+    });
+    return winner === undefined ? undefined : winner.year;
+}
